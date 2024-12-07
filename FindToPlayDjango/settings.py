b9f4832a4be64d4fcb676a438f496e95796f1959
bluzer <bluzer@yandex.ru>
2024-12-07 13:56:17 +0300

    postrgres + try to add infra

--- FindToPlayDjango/settings.py
+++ FindToPlayDjango/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +85,12 @@ WSGI_APPLICATION = 'FindToPlayDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_NAME'),
+        'USER':  os.getenv('PG_USER'),
+        'PASSWORD':  os.getenv('PG_PASSWORD'),
+        'HOST':  os.getenv('PG_HOST'),
+        'PORT':  os.getenv('PG_PORT'),
     }
 }
 
